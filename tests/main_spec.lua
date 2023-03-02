025c8a56454c34cd762f678140b4eb1035d872c2
Khachig <ainteblian99@gmail.com>
2023-03-02 01:02:59 -0500

    Update main-spec test for new setup API

--- tests/main_spec.lua
+++ tests/main_spec.lua
@@ -1,10 +1,24 @@
 vim.api.nvim_command("set rtp+=.")
 local data_path = vim.fn.stdpath("data")
 
+local Path = require("plenary.path")
+local CONFIG = Path:new(vim.fn.stdpath("data")):joinpath("notation-data")
+local BACKUP = Path:new(vim.fn.stdpath("data")):joinpath("notation-data.bak")
+
 local function clean_setup()
     package.loaded["notation"] = nil
     vim.fn.system("rm -rf /tmp/Notes")
     vim.fn.system("mkdir -p /tmp/Notes/Inbox")
+    if CONFIG:exists() then
+        vim.fn.system("mv " .. CONFIG.filename .. " " .. BACKUP.filename)
+    end
+end
+
+local function restore_setup()
+    vim.fn.system("rm " .. CONFIG.filename)
+    if BACKUP:exists() then
+        vim.fn.system("mv " .. BACKUP.filename .. " " .. CONFIG.filename)
+    end
 end
 
 describe("notation.setup", function()
@@ -15,10 +29,13 @@ describe("notation.setup", function()
         nsb = require("notation")
     end)
 
+    after_each(function()
+        restore_setup()
+    end)
+
     it("sets root path", function()
         nsb.setup({
-            notes_root="/tmp/Notes/",
-            notes_inbox="/tmp/Notes/Inbox/"
+            notes_dir="/tmp/Notes",
         })
 
         local notes_paths = vim.fn.readfile(data_path .. "/notation-data")
