460f5dc3800bdfc4cc3a29fab580fe72e2357918
Khachig Ainteblian <k_hachig@Khachigs-MacBook-Pro.local>
2023-03-10 23:38:47 -0500

    Refactor dev/init.lua to be more ergonomic and include setup/teardown

--- dev/init.lua
+++ dev/init.lua
@@ -1,8 +1,24 @@
+local function reset()
+    package.loaded["notation"] = nil
+    package.loaded["notation.main"] = nil
+    package.loaded["dev"] = nil
+end
+
+CONFPATH = vim.fn.stdpath("data") .. "/notation-data"
+
+local function setup()
+    reset()
+    os.execute("mv " .. CONFPATH .. " " .. CONFPATH .. ".bak 2> /dev/null")
+    require("notation").setup({notes_dir="/tmp/Notes", default_keymaps=false})
+end
+
+local function teardown()
+    reset()
+    os.execute("rm -r /tmp/Notes 2> /dev/null")
+    os.execute("rm " .. CONFPATH .. " 2> /dev/null")
+    os.execute("mv " .. CONFPATH .. ".bak" .. " " .. CONFPATH)
+end
+
 vim.api.nvim_command("set rtp+=.")
-package.loaded["notation"] = nil
-package.loaded["notation.main"] = nil
-package.loaded["dev"] = nil
-
-require("notation").setup({notes_dir="/tmp/Notes", default_keymaps=false})
-
-vim.api.nvim_set_keymap("n", "<Space>R", ":luafile dev/init.lua<CR>", {})
+vim.keymap.set("n", "<Space>RS", setup)
+vim.keymap.set("n", "<Space>RR", teardown)
