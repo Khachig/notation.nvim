cd0ae021db37bc25df1de5f34dc9e95b240da2de
Khachig Ainteblian <k_hachig@Khachigs-MacBook-Pro.local>
2023-03-11 01:43:11 -0500

    Modify telescope utility functions to be more ergonomic

--- lua/notation/utils.lua
+++ lua/notation/utils.lua
@@ -2,52 +2,6 @@ local Path = require("plenary.path")
 
 local M = {}
 
-function M.get_notes_dirs(notes_dir)
-    local config = Path:new(vim.fn.stdpath("data")):joinpath("notation-data")
-    local root, inbox
-    if config:exists() then
-        local lines = vim.fn.readfile(config.filename)
-        root = Path:new(lines[1])
-        inbox = Path:new(lines[2])
-    else
-        root = Path:new(vim.fn.expand(notes_dir or "~/Notes"))
-        inbox = root:joinpath("Inbox")
-        vim.fn.writefile({root.filename, inbox.filename}, config.filename)
-    end
-    if root:exists() == false or inbox:exists() == false then
-        vim.fn.mkdir(inbox.filename, "p")
-    end
-    return root, inbox
-end
-
-function M.show_telescope_picker (picker_list, opts)
-    local pickers = require("telescope.pickers")
-    local finders = require("telescope.finders")
-    local conf = require("telescope.config").values
-    local previewer = require("telescope.previewers").new_buffer_previewer
-    opts = opts or {}
-    local preview
-    if opts.preview then
-        preview = previewer({
-            define_preview=function(self, entry)
-                conf.buffer_previewer_maker(opts.preview_root_path:joinpath(entry[1]).filename, self.state.bufnr, {
-                    bufname = self.state.bufname,
-                    winid = self.state.winid,
-                })
-            end
-        })
-    end
-    pickers.new(opts, {
-        results_title = opts.results_title,
-        default_text= opts.default_text,
-        finder = finders.new_table {
-            results = picker_list
-        },
-        sorter = conf.generic_sorter(opts),
-        previewer = preview
-    }):find()
-end
-
 function M.get_telescope_opts(options)
     local actions = require("telescope.actions")
     local action_state = require("telescope.actions.state")
@@ -73,6 +27,53 @@ function M.get_telescope_opts(options)
     end
 
     return opts
+end
+
+function M.get_notes_dirs(notes_dir)
+    local config = Path:new(vim.fn.stdpath("data")):joinpath("notation-data")
+    local root, inbox
+    if config:exists() then
+        local lines = vim.fn.readfile(config.filename)
+        root = Path:new(lines[1])
+        inbox = Path:new(lines[2])
+    else
+        root = Path:new(vim.fn.expand(notes_dir or "~/Notes"))
+        inbox = root:joinpath("Inbox")
+        vim.fn.writefile({root.filename, inbox.filename}, config.filename)
+    end
+    if root:exists() == false or inbox:exists() == false then
+        vim.fn.mkdir(inbox.filename, "p")
+    end
+    return root, inbox
+end
+
+function M.show_telescope_picker (picker_list, opts)
+    local pickers = require("telescope.pickers")
+    local finders = require("telescope.finders")
+    local conf = require("telescope.config").values
+    local previewer = require("telescope.previewers").new_buffer_previewer
+    local preview
+    opts = M.get_telescope_opts(opts or {})
+    if opts.preview then
+        preview = previewer({
+            define_preview=function(self, entry)
+                vim.notify(opts.preview_root_path:joinpath(entry[1]).filename, vim.log.levels.DEBUG)
+                conf.buffer_previewer_maker(opts.preview_root_path:joinpath(entry[1]).filename, self.state.bufnr, {
+                    bufname = self.state.bufname,
+                    winid = self.state.winid,
+                })
+            end
+        })
+    end
+    pickers.new(opts, {
+        results_title = opts.results_title,
+        default_text= opts.default_text,
+        finder = finders.new_table {
+            results = picker_list
+        },
+        sorter = conf.generic_sorter(opts),
+        previewer = preview
+    }):find()
 end
 
 return M
